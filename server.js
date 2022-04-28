99f9be748e1261fbc2713698931a95baad39d914
IsaiasQB <isaias.qb@hotmail.com>
2022-04-28 18:58:11 -0400

    added route for index.html

--- server.js
+++ server.js
@@ -129,6 +129,11 @@ app.post('/api/animals', (req, res) => {
 });
 
 
+//ROUTE for index.html to be served from our Express.js server.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`)
 })
