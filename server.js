59f0308d09428edda0d7bf7083f65970e251e754
IsaiasQB <isaias.qb@hotmail.com>
2022-04-28 18:18:37 -0400

    added validation for entering new animal data

--- server.js
+++ server.js
@@ -65,9 +65,32 @@ function findById(id, animalsArray) {
 
 function createNewAnimal(body, animalsArray) {
   const animal = body;
-  animalsArray.push(animal)
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'), 
+    JSON.stringify({ animals: animalsArray}, null, 2)
+  );
 
   return animal
+}
+
+
+// VALIDATION function
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -94,9 +117,15 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set ID for new animal data, based on what the next index of the array will be
   req.body.id = animals.length.toString()
+
+  //if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
     //add animal to json file and animals array in this function
   const animal = createNewAnimal(req.body, animals);
   res.json(animal);  //this sends the data back to the client
+  } 
 });
 
 
